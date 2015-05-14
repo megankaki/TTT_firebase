f0bc9ed3e7c100514e163474cddacc274c8884bf
Megan Kakitsubo <megankaki@gmail.com>
2015-05-13 18:55:23 -0700

    set up firebase, made board, ng-repeat ng-click, ng-bind in index.html, and controller.js created counter to track turn change, made function changeValue to process turn change and input x or o depending on turn, created gridlist in variable

---
+++ js/controller.js
@@ -0,0 +1,42 @@
+angular
+	.module('TTTapp')
+	.controller('TTTController', TTTController)
+
+TTTController.$inject = ['$firebaseObject']
+
+function TTTController($firebaseObject){
+
+	var self = this;
+	self.whatever = whatever();
+	self.counter = 0;
+	self.changeValue = changeValue;
+
+	self.game =  {
+		gridlist: ["","","","","","","","",""]
+	};
+	// , turnCounter: 0;
+
+	
+	function whatever(){
+		var ref = new Firebase("https://tic-tac-toe-mk.firebaseio.com/");
+		var whatever = $firebaseObject(ref);
+		return whatever;
+	}
+
+	function changeValue(e){
+		if (self.counter % 2 == 0){
+			//player1's turn
+			self.game.gridlist[e]="x";
+			self.counter++;
+			// console.log(self.counter);
+
+		}else if(self.counter % 2 !== 0){
+			//player2's turn;
+			self.game.gridlist[e]="o"
+			self.counter++;
+			// console.log(self.counter);
+		}
+		console.log(e)
+	}
+
+}
