21b4b0aae4483652421f5e7a420530d40b647fae
Megan Kakitsubo <megankaki@gmail.com>
2015-05-14 14:17:09 -0700

    cleaned up syntax

--- js/controller.js
+++ js/controller.js
@@ -2,41 +2,49 @@ angular
 	.module('TTTapp')
 	.controller('TTTController', TTTController)
 
-TTTController.$inject = ['$firebaseObject']
+	TTTController.$inject = ['$firebaseObject']
 
-function TTTController($firebaseObject){
+	function TTTController($firebaseObject) {
 
-	var self = this;
-	self.whatever = whatever();
-	self.counter = 0;
-	self.changeValue = changeValue;
+		var self = this;
+			self.whatever = whatever();
+			self.counter = 0;
+			self.changeValue = changeValue;
+			self.game = {
+				gridlist: ["","","","","","","","",""]
+			};
+			// , turnCounter: 0;
 
-	self.game =  {
-		gridlist: ["","","","","","","","",""]
-	};
-	// , turnCounter: 0;
+		function whatever() {
 
-	
-	function whatever(){
-		var ref = new Firebase("https://tic-tac-toe-mk.firebaseio.com/");
-		var whatever = $firebaseObject(ref);
-		return whatever;
-	}
+			var ref = new Firebase("https://tic-tac-toe-mk.firebaseio.com/");
+			var whatever = $firebaseObject(ref);
+				return whatever;
 
-	function changeValue(e){
-		if (self.counter % 2 == 0){
-			//player1's turn
-			self.game.gridlist[e]="x";
-			self.counter++;
-			// console.log(self.counter);
-
-		}else if(self.counter % 2 !== 0){
-			//player2's turn;
-			self.game.gridlist[e]="o"
-			self.counter++;
-			// console.log(self.counter);
 		}
-		console.log(e)
+
+		function changeValue(e) {
+
+			if (self.counter % 2 == 0) {
+				//player1's turn
+				self.game.gridlist[e] = "x";
+				self.counter++;
+				self.whatever.$add({ 
+					value:'empty' 
+				});
+				self.whatever.$save();
+				// console.log(self.counter);
+			} 
+			else if (self.counter % 2 !== 0) {
+				//player2's turn;
+				self.game.gridlist[e] = "o"
+				self.counter++;
+				// console.log(self.counter);
+			}
+			console.log(e)
+
+		}
+
 	}
 
-}
+
