6f2ae134c2eb3e3313c483ec281d0b0668a33633
Megan Kakitsubo <megankaki@gmail.com>
2015-05-15 14:00:11 -0700

    added button that submits name for both players

--- js/controller.js
+++ js/controller.js
@@ -8,9 +8,11 @@ angular
 
 		var self = this;
 			self.whatever = whatever();
-			self.counter = 0;
 			self.changeValue = changeValue;
-			self.turn = "x";			
+			self.winner = winner;
+			self.nameChange1 = nameChange1;
+			self.nameChange2 = nameChange2;
+			self.clearBoard = clearBoard;
 
 		//hooks firebase to code. one time only.
 		function whatever() {
@@ -25,6 +27,16 @@ angular
 	    self.whatever.$loaded (
 
 	    	function() {
+	    		
+		        self.whatever.counter = 1;
+		        self.whatever.p1score = 0;
+		        self.whatever.p2score = 0;
+		        self.whatever.name1 = "Player 1"
+		        self.whatever.name2 = "Player 2" 	 
+		        self.whatever.winner = "Who will be the winner?";
+        		self.whatever.showName1 = true;
+        		self.whatever.showName2 = true;
+        		self.whatever.showWinner = false; 
 	    		//each object stored in array called 'gridList'
 		        self.whatever.gridList = [
 					{
@@ -55,51 +67,128 @@ angular
 		                value: "" 
 		            }
 				];
-		            
-		            self.whatever.$save();
+            
+		        self.whatever.$save();
         	}
         );
-
+	
+		    function nameChange1() {
+	            self.whatever.showName1 = !self.whatever.showName1;
+	            self.whatever.$save();
+        	}
+		    
+		    function nameChange2() {
+	            self.whatever.showName2 = !self.whatever.showName2;
+	            self.whatever.$save();
+        	}
 
 		//function that inputs "x" or "o", changes turn, updates firebase
-		function changeValue(e) {
+		//...make sure that double click does not work. even number view-> only player1
+	    	function changeValue($index) {                 
+	            if (
+	                self.whatever.counter % 2 !== 0  
+	                && self.whatever.gridList[$index].value == ""
+	            ) {
+	                console.log(self.whatever.gridList[$index])
+	                self.whatever.counter++;
+	                self.whatever.gridList[$index].value = "x";
+	            } 
+	            else if (
+	                self.whatever.counter % 2 == 0 
+	                && self.whatever.gridList[$index].value == ""
+	            ) {
+	                console.log(self.whatever.gridList[$index])
+	                self.whatever.counter++;
+	                self.whatever.gridList[$index].value = "o";
+	            }
+				self.winner();
+	            self.whatever.$save();
+	        }	
+	   	//function that runs win logic
+	   		function winner() {
+				console.log("winner function is running")
+				var tokens = ["x", "o"];
+				for (var i = 0; i < tokens.length; i++){
+					var t = tokens[i];
+				
+					//Check for a winner horizontally
+					if ( ((self.whatever.gridList[0].value == t) && (self.whatever.gridList[1].value == t) && (self.whatever.gridList[2]).value == t) ||
+						((self.whatever.gridList[3].value == t) && (self.whatever.gridList[4].value == t) && (self.whatever.gridList[5]).value == t) || 
+						((self.whatever.gridList[6].value == t) && (self.whatever.gridList[7].value == t) && (self.whatever.gridList[8]).value == t) || 
+						((self.whatever.gridList[0].value == t) && (self.whatever.gridList[3].value == t) && (self.whatever.gridList[6]).value == t) || 
+						((self.whatever.gridList[1].value == t) && (self.whatever.gridList[4].value == t) && (self.whatever.gridList[7]).value == t) || 
+						((self.whatever.gridList[2].value == t) && (self.whatever.gridList[5].value == t) && (self.whatever.gridList[8]).value == t) || 
+						((self.whatever.gridList[0].value == t) && (self.whatever.gridList[4].value == t) && (self.whatever.gridList[8]).value == t) || 
+						((self.whatever.gridList[2].value == t) && (self.whatever.gridList[4].value == t) && (self.whatever.gridList[6]).value == t) )  {
 
-			console.log("clicked")
-				//if empty grid and player's turn is "x"...
-				if (
-					self.whatever.gridList[e].value == "" 
-					&& self.turn == "x"
-				){
-					//change grid to "x"
-					self.whatever.gridList[e].value = "x"; 
-					//"x" in console
-					console.log(self.whatever.gridList[e].value); 
-					//changes turn to "o"
-					self.turn = "o";
-					//"next is o's turn" in console
-					console.log("next is " + self.turn + "'s turn")
-				} 
-				// if empty grid and player's turn is "o"...
-				else if (
-					self.whatever.gridList[e].value == "" 
-					&& self.turn == "o"
-				){
-					//change grid to "o"
-					self.whatever.gridList[e].value = "o";
-					//"o" in console
-					console.log(self.whatever.gridList[e].value);
-					//changes turn to "x"
-					self.turn = "x";
-					//"next is x's turn" in console
-					console.log("next is " + self.turn + "'s turn");		
-				} 
+						if (t == "x"){
+							console.log("Player 1 Wins!");
+							self.whatever.p1score++;
+							self.whatever.winner = self.whatever.name1 + " wins";
 
-			//updates information in firebase
-			self.whatever.$save();	
-		}
+						} else {
+							console.log("Player 2 Wins!");
+							self.whatever.p2score++;
+							self.whatever.winner = self.whatever.name2 + " wins";
+						}
+						self.whatever.showWinner = true;
+						self.whatever.$save();
+						self.delayClearBoard();
+					}
+				}
+
+				if (self.whatever.counter == 9){
+					self.whatever.winner = "It's a tie! No one wins";
+					self.whatever.showWinner = true;
+					self.delayClearBoard();
+				}
+			}
+
+			function clearBoard() {
+	    		//clears Board
+		        self.whatever.gridList = [
+					{
+		                value: ""
+		            } , 
+		            {
+		                value: ""
+		            } , 
+		            {
+		                value: ""
+		            } , 
+		            {
+		                value: ""
+		            } , 
+		            {
+		                value: ""
+		            } , 
+		            {
+		                value: ""
+		            } , 
+		            {
+		                value: ""
+		            } , 
+		            {
+		                value: ""
+		            } , 
+		            {
+		                value: "" 
+		            }
+				];
+		        self.whatever.counter = 0; 
+		    	self.whatever.winner = "Who'll win next?";
+        		self.whatever.showWinner = false;	        		             
+		        self.whatever.$save();
+        	}
 
 
+			self.delayClearBoard = delayClearBoard;
 
-	}
+			function delayClearBoard() {
+				setTimeout(function(){
+					self.clearBoard()
+				}, 2000)
+			}
 
 
+}
