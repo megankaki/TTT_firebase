bb7f6eec8a19b9b3a51e674b9bc8c421fc46bd9c
Megan Kakitsubo <megankaki@gmail.com>
2015-05-14 17:58:30 -0700

    controller.js added gridList as array and added 9 objects. each object has a string. changed the changeValue function with turn change determined on self.turn = x or o. call save at the end of changeValue

--- js/controller.js
+++ js/controller.js
@@ -10,11 +10,9 @@ angular
 			self.whatever = whatever();
 			self.counter = 0;
 			self.changeValue = changeValue;
-			self.game = {
-				gridlist: ["","","","","","","","",""]
-			};
-			// , turnCounter: 0;
+			self.turn = "x";			
 
+		//hooks firebase to code. one time only.
 		function whatever() {
 
 			var ref = new Firebase("https://tic-tac-toe-mk.firebaseio.com/");
@@ -23,27 +21,84 @@ angular
 
 		}
 
+		//when page loads, create 9 objects in firebase.
+	    self.whatever.$loaded (
+
+	    	function() {
+	    		//each object stored in array called 'gridList'
+		        self.whatever.gridList = [
+					{
+		                value: ""
+		            } , 
+		            {
+		                value: ""
+		            } , 
+		            {
+		                value: ""
+		            } , 
+		            {
+		                value: ""
+		            } , 
+		            {
+		                value: ""
+		            } , 
+		            {
+		                value: ""
+		            } , 
+		            {
+		                value: ""
+		            } , 
+		            {
+		                value: ""
+		            } , 
+		            {
+		                value: "" 
+		            }
+				];
+		            
+		            self.whatever.$save();
+        	}
+        );
+
+
+		//function that inputs "x" or "o", changes turn, updates firebase
 		function changeValue(e) {
 
-			if (self.counter % 2 == 0) {
-				//player1's turn
-				self.game.gridlist[e] = "x";
-				self.counter++;
-				self.whatever.$add({ 
-					value:'empty' 
-				});
-				self.whatever.$save();
-				// console.log(self.counter);
-			} 
-			else if (self.counter % 2 !== 0) {
-				//player2's turn;
-				self.game.gridlist[e] = "o"
-				self.counter++;
-				// console.log(self.counter);
-			}
-			console.log(e)
+			console.log("clicked")
+				//if empty grid and player's turn is "x"...
+				if (
+					self.whatever.gridList[e].value == "" 
+					&& self.turn == "x"
+				){
+					//change grid to "x"
+					self.whatever.gridList[e].value = "x"; 
+					//"x" in console
+					console.log(self.whatever.gridList[e].value); 
+					//changes turn to "o"
+					self.turn = "o";
+					//"next is o's turn" in console
+					console.log("next is " + self.turn + "'s turn")
+				} 
+				// if empty grid and player's turn is "o"...
+				else if (
+					self.whatever.gridList[e].value == "" 
+					&& self.turn == "o"
+				){
+					//change grid to "o"
+					self.whatever.gridList[e].value = "o";
+					//"o" in console
+					console.log(self.whatever.gridList[e].value);
+					//changes turn to "x"
+					self.turn = "x";
+					//"next is x's turn" in console
+					console.log("next is " + self.turn + "'s turn");		
+				} 
 
+			//updates information in firebase
+			self.whatever.$save();	
 		}
+
+
 
 	}
 
